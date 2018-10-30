7cf033ac5c9ccf2fc41074014844dc494ec9db84
kirbubu <38482025+kirbubu@users.noreply.github.com>
2018-10-30 00:30:15 -0700

    Added Project
    Starting work for the project (may not compile)

---
+++ CECS 475 Assignment 6/CECS 475 Assignment 6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CECS_475_Assignment_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
