9d4ee35f273ae79eddb2c254105665e25cff3ad5
kirbubu <38482025+kirbubu@users.noreply.github.com>
2018-10-30 17:44:31 -0700

    Reupload Project (again)
    Took source code and created methods regarding StudentRepository in Business Layer

---
+++ Assignment5/BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,86 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace BusinessLayer
+{
+    public class BusinessLayer : IBusinessLayer
+    {
+        private readonly IStandardRepository _standardRepository;
+        private readonly IStudentRepository _studentRepository;
+
+        public BusinessLayer()
+        {
+            _standardRepository = new StandardRepository();
+            _studentRepository = new StudentRepository();
+        }
+
+        #region Standard
+        public IEnumerable<Standard> GetAllStandards()
+        {
+            return _standardRepository.GetAll();
+        }
+
+        public Standard GetStandardByID(int id)
+        {
+            return _standardRepository.GetById(id);
+        }
+
+        public Standard GetStandardByName(string name)
+        {
+            return _standardRepository.GetSingle(
+                s => s.StandardName.Equals(name),
+                s => s.Students);
+        }
+
+        public void AddStandard(Standard standard)
+        {
+            _standardRepository.Insert(standard);
+        }
+
+        public void UpdateStandard(Standard standard)
+        {
+            _standardRepository.Update(standard);
+        }
+
+        public void RemoveStandard(Standard standard)
+        {
+            _standardRepository.Delete(standard);
+        }
+        #endregion
+
+        #region Student
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _studentRepository.GetAll();
+        }
+
+        public Student GetStudentByID(int id)
+        {
+            return _studentRepository.GetById(id);
+        }
+
+        public void AddStudent(Student student)
+        {
+            _studentRepository.Insert(student);
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            _studentRepository.Update(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            _studentRepository.Delete(student);
+        }
+        #endregion
+
+        #region Teachers
+
+        #endregion
+
+
+    }
+}
---
+++ Assignment5/BusinessLayer/IBusinessLayer.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public interface IBusinessLayer
+    {
+        #region Standard
+        IEnumerable<Standard> GetAllStandards();
+
+        Standard GetStandardByID(int id);
+
+        Standard GetStandardByName(string name);
+
+        void AddStandard(Standard standard);
+
+        void UpdateStandard(Standard standard);
+
+        void RemoveStandard(Standard standard);
+        #endregion
+
+        #region Student
+        IEnumerable<Student> GetAllStudents();
+
+        Student GetStudentByID(int id);
+
+        void AddStudent(Student st);
+
+        void UpdateStudent(Student st);
+
+        void RemoveStudent(Student st);
+        #endregion
+    }
+}
